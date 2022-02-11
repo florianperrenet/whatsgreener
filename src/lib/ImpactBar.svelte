b2ef46d6a7168e17d21ff7a5341cb11d0f01116d
florian <florian.perrenet@gmail.com>
2022-02-11 12:39:30 +0100

    fixed impactbar dynamic color not showing

--- src/lib/ImpactBar.svelte
+++ src/lib/ImpactBar.svelte
@@ -12,6 +12,7 @@
     if (value.lte(dec("10"))) color = "green";
     else if (value.lte(dec("50"))) color = "orange";
     else color = "red";
+    return color;
     return `bg-${color}-600`;
   }
 </script>
@@ -19,6 +20,9 @@
 <div class="flex items-center overflow-hidden">
   <div class="flex-none mr-2 text-sm">WGI: {value.toFixed(2)}</div>
   <div class="flex-initial w-full bg-gray-200 rounded-full h-2.5">
-    <div class="h-2.5 rounded-full {getColor(value)}" style="width: {value}%" />
+    <div
+      class="h-2.5 rounded-full"
+      style="background-color: {getColor(value)}; width: {value}%"
+    />
   </div>
 </div>
