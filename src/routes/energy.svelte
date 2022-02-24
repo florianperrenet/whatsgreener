312651799740de19c29df255718d9536d514b42b
florian <florian.perrenet@gmail.com>
2022-02-24 12:09:39 +0100

    fixed energy mixture per capita

--- src/routes/energy.svelte
+++ src/routes/energy.svelte
@@ -4,17 +4,19 @@
   import Select from "$lib/Select.svelte";
 
   let energyMix = {};
-  let countries = [];
+  let entities = [];
   let selected = null;
 
   let year = "2019";
 
   onMount(async () => {
-    const response = await fetch("/static/iea_country_energy_mix.json");
+    const response = await fetch(
+      "/resources/ourworldindata-energy-mix-per-capita.json"
+    );
     const data = await response.json();
     energyMix = data;
-    countries = energyMix.countries.map((country) => [country, country]);
-    selected = countries[0][0];
+    entities = energyMix.entities.map((entity) => [entity, entity]);
+    selected = entities[0][0];
   });
 </script>
 
@@ -22,14 +24,18 @@
   <div class="prose lg:prose-lg prose-slate">
     <div class="text-7xl font-bold text-gray-900 mb-20">Energy footprint</div>
 
-    <h1>Energy mixture per country</h1>
+    <h1>Energy mixture per capita</h1>
     <div class="not-prose">
-      <Select text="Country" bind:selected options={countries} />
+      <Select text="Capita" bind:selected options={entities} />
     </div>
 
     {#if energyMix.data}
       {#each Object.entries(energyMix.data[selected][year]) as [key, value]}
-        <li>{key}: {value.amount} PJ = {value.percentage_r}%</li>
+        {#if key === "total"}
+          <li>Total: {value}</li>
+        {:else}
+          <li>{key}: {value.a} kWh = {value.pr}%</li>
+        {/if}
       {/each}
     {/if}
 
