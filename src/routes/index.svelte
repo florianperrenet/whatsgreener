34e932a86569dc19f65b27aef7f597de588bcb25
florian <florian.perrenet@gmail.com>
2022-03-01 12:45:04 +0100

    homepage improvement

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -3,6 +3,103 @@
 </script>
 
 <script>
+	import { onMount } from "svelte";
+
+	import * as d3 from "d3";
+	import * as topojson from "topojson-client";
+
+	const size = 600;
+
+	onMount(async () => {
+		var canvas = document.getElementById("my_dataviz");
+
+		const projection = d3
+			// .geoEqualEarth()
+			.geoOrthographic();
+		// .geoNaturalEarth1()
+		// .scale(width / 1.3 / Math.PI)
+		// .translate([width / 2, height / 2]);
+
+		const context = canvas.getContext("2d");
+		const path = d3.geoPath(projection, context);
+
+		const response = await fetch("/land-50m.json");
+		const world = await response.json();
+
+		const outline = { type: "Sphere" };
+		const graticule = d3.geoGraticule10();
+		const land = topojson.feature(world, world.objects.land);
+
+		const width = size;
+		const height = (() => {
+			const [[x0, y0], [x1, y1]] = d3
+				.geoPath(projection.fitWidth(width, outline))
+				.bounds(outline);
+			const dy = Math.ceil(y1 - y0),
+				l = Math.min(Math.ceil(x1 - x0), dy);
+			projection.scale((projection.scale() * (l - 1)) / l).precision(0.2);
+			return dy;
+		})();
+
+		// const context = canvas.context2d(width, height);
+
+		context.save();
+		context.beginPath(),
+			path(outline),
+			context.clip(),
+			(context.fillStyle = "#fff"),
+			context.fillRect(0, 0, width, height);
+
+		context.beginPath(),
+			path(graticule),
+			(context.strokeStyle = "#ccc"),
+			context.stroke();
+
+		context.beginPath(),
+			path(land),
+			(context.fillStyle = "#000"),
+			context.fill();
+
+		context.restore();
+		context.beginPath(),
+			path(outline),
+			(context.strokeStyle = "#000"),
+			context.stroke();
+
+		// // select the canvas element created in the html.
+		// var canvas = document.getElementById("my_dataviz");
+		// // Actual width and height. No idea if clienWidth would be a better option..?
+		// var width = canvas.offsetWidth;
+		// var height = canvas.offsetHeight;
+		// // Set a projection for the map. Projection = transform a lat/long on a position on the 2d map.
+		// var projection = d3
+		// 	.geoEqualEarth()
+		// 	// .geoNaturalEarth1()
+		// 	.scale(width / 1.3 / Math.PI)
+		// 	.translate([width / 2, height / 2]);
+		// // Get the 'context'
+		// var ctx = canvas.getContext("2d");
+		// // geographic path generator for given projection and canvas context
+		// const pathGenerator = d3.geoPath(projection, ctx);
+		// // Draw a background
+		// // ctx.fillStyle = '#ddd';
+		// // ctx.fillRect(0, 0, width, height);
+		// // Load external data and boot
+		// const response = await fetch(
+		// 	"https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/world.geojson"
+		// );
+		// const data = await response.json();
+		// // initialize the path
+		// ctx.beginPath();
+		// // Got the positions of the path
+		// pathGenerator(data);
+		// // Fill the paths
+		// ctx.fillStyle = "#999";
+		// ctx.fill();
+		// // Add stroke
+		// // ctx.strokeStyle = "#69b3a2";
+		// // ctx.stroke();
+	});
 </script>
 
 <svelte:head>
@@ -59,6 +156,11 @@
 		</div> -->
 		<!-- style="background: url('/screenshot.png'); background-size: cover; height: 700px;" -->
 
+		<div class="xl:absolute xl:top-60 xl:bottom-0 xl:right-0 xl:w-1/2">
+			<div class="w-full">
+				<canvas id="my_dataviz" width={size} height={size} />
+			</div>
+		</div>
 		<!-- <div class="h-40" />
 
 		<hr class="mt-10 mb-10" />
