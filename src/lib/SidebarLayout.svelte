4737593850ed8df2437c4f0c5d833b4373307b6c
florian <florian.perrenet@gmail.com>
2022-03-03 20:31:38 +0100

    improved calculations sidebar

--- src/lib/SidebarLayout.svelte
+++ src/lib/SidebarLayout.svelte
@@ -3,33 +3,26 @@
 </script>
 
 {#if fixed}
-  <div class="relative flex w-full">
+  <div class="relative w-full bg-white lg:bg-gray-50">
     <div
       class="hidden lg:block absolute top-0 bottom-0 right-0 left-1/2 bg-white"
     />
-    <div
-      class="relative flex w-full max-w-container mx-auto px-4 sm:px-6 lg:px-8"
-    >
-      <div class="w-full flex-none lg:grid lg:grid-cols-3 lg:gap-8">
-        <div
-          class="bg-gray-50 lg:bg-transparent -mx-4 sm:-mx-6 lg:mx-0 py-12 sm:py-16 px-4 sm:px-6 lg:pl-0 lg:pr-8"
-        >
-          <nav
-            class="overflow-y-auto block fixed inset-0 top-16 left-[max(0px,calc(50%-42.5rem))] right-auto w-[calc(83rem/3)] pb-10 px-8"
-          >
-            <div class="relative">
-              <slot name="sidebar" />
-            </div>
-          </nav>
+    <div class="max-w-container mx-auto px-4 sm:px-6 lg:px-8">
+      <nav
+        class="hidden lg:block overflow-y-auto fixed inset-0 top-16 left-[max(0px,calc(50%-42.5rem))] right-auto w-[20rem] lg:w-[22.5rem] pb-10 px-8"
+      >
+        <div class="relative">
+          <slot name="sidebar" />
         </div>
-        <div class="relative col-span-2 lg:-ml-8 bg-white lg:shadow-md">
+      </nav>
+
+      <div class="lg:pl-[20.5rem]">
+        <div class="relative mx-auto py-16 lg:px-16 bg-white lg:shadow-md">
           <div
             class="hidden lg:block absolute top-0 bottom-0 -right-4 w-8 bg-white"
           />
-          <div class="relative py-16 lg:px-16">
-            <div class="prose lg:prose-lg prose-slate mx-auto">
-              <slot name="content" />
-            </div>
+          <div class="prose lg:prose-lg prose-slate mx-auto">
+            <slot name="content" />
           </div>
         </div>
       </div>
