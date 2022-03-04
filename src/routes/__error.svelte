5368894d02db79ccfffc4f38881d1eee1a375e10
florian <florian.perrenet@gmail.com>
2022-03-04 17:08:47 +0100

    added error page

---
+++ src/routes/__error.svelte
@@ -0,0 +1,19 @@
+<script context="module">
+  /** @type {import('@sveltejs/kit').ErrorLoad} */
+  export function load({ error, status }) {
+    return {
+      props: {
+        title: `${status}: ${error.message}`,
+      },
+    };
+  }
+</script>
+
+<script>
+  import ContainerLayout from "$lib/ContainerLayout.svelte";
+  export let title;
+</script>
+
+<ContainerLayout>
+  <h1 class="font-medium text-4xl">{title}</h1>
+</ContainerLayout>
