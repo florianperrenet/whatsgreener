ef89dae9f811a777c8ae1f387054c7c97b2881a1
florian <florian.perrenet@gmail.com>
2022-03-11 21:48:19 +0100

    improved popover with simple trick

--- src/lib/components/PopOver.svelte
+++ src/lib/components/PopOver.svelte
@@ -11,19 +11,11 @@
   let popover;
   let popover_arrow;
 
-  let show_popover = false;
-  let popover_focus = false;
-
-  let going_to_remove = false;
-
-  let time_showed;
-
   function onMouseEnter() {
-    going_to_remove = false;
+    show();
+  }
 
-    time_showed = new Date();
-
-    show_popover = true;
+  function show() {
     if (!popover) return;
     popover.style.display = "block";
 
@@ -61,31 +53,12 @@
   }
 
   function hide() {
-    show_popover = false;
     if (!popover) return;
     popover.style.display = "";
   }
 
-  function removeWithTimeout() {
-    going_to_remove = true;
-    setTimeout(() => {
-      if (going_to_remove && show_popover && !popover_focus) {
-        hide();
-      }
-    }, 200);
-  }
-
   function onMouseLeave(e) {
-    removeWithTimeout();
-  }
-
-  function contentEnter(e) {
-    popover_focus = true;
-  }
-
-  function contentLeave(e) {
-    popover_focus = false;
-    removeWithTimeout();
+    hide();
   }
 </script>
 
@@ -95,22 +68,18 @@
   on:mouseleave={onMouseLeave}
 >
   <slot name="element" />
-</span>
 
-<div
-  bind:this={popover}
-  class="absolute hidden rounded bg-white shadow-md ring-1 ring-gray-900 ring-opacity-5"
->
   <div
-    class="pointer-events-none absolute h-[12px] w-[12px] bg-gray-300 after:absolute after:bottom-[1px] after:left-[1px] after:h-[12px] after:w-[12px] after:bg-white"
-    style="transform: rotate(-45deg);"
-    bind:this={popover_arrow}
-  />
-  <div
-    class="bg-red -mb-2 px-3 pt-2 pb-4"
-    on:mouseenter={contentEnter}
-    on:mouseleave={contentLeave}
+    bind:this={popover}
+    class="absolute hidden rounded bg-white shadow-md ring-1 ring-gray-900 ring-opacity-5"
   >
-    <slot name="content" />
+    <div
+      class="absolute h-[12px] w-[12px] bg-gray-300 after:absolute after:bottom-[1px] after:left-[1px] after:h-[12px] after:w-[12px] after:bg-white"
+      style="transform: rotate(-45deg);"
+      bind:this={popover_arrow}
+    />
+    <div class="bg-red -mb-2 px-3 pt-2 pb-4">
+      <slot name="content" />
+    </div>
   </div>
-</div>
+</span>
