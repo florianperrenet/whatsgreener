e1563130fb8545b85f7a73a9041cbffdea1cf8e8
florian <florian.perrenet@gmail.com>
2022-03-15 10:00:03 +0100

    improved RefOrEdit

--- src/lib/components/Ref.svelte
+++ src/lib/components/Ref.svelte
@@ -7,7 +7,7 @@
     <a href="#" target="_blank" class="flex items-center text-sm text-blue-600">${to}
     <svg
         xmlns="http://www.w3.org/2000/svg"
-        class="ml-2 h-4 w-4"
+        class="ml-1 h-4 w-4"
         fill="none"
         viewBox="0 0 24 24"
         stroke="currentColor"
--- src/lib/components/RefOrEdit.svelte
+++ src/lib/components/RefOrEdit.svelte
@@ -6,24 +6,13 @@
   export let value;
 
   if (value === undefined) {
-    to = "value missing";
+    to = "edit";
   }
 
   const content = `
     <a href="#" target="_blank" class="flex items-center text-sm text-blue-600">${to}
-    <svg
-        xmlns="http://www.w3.org/2000/svg"
-        class="ml-2 h-4 w-4"
-        fill="none"
-        viewBox="0 0 24 24"
-        stroke="currentColor"
-        stroke-width="2"
-      >
-        <path
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14"
-        />
+      <svg xmlns="http://www.w3.org/2000/svg" class="ml-1 h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+        <path stroke-linecap="round" stroke-linejoin="round" d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z" />
       </svg>
     </a>
   `;
